ecda8825702c3e6040ef41130a894e7b788a7cf3
Varvara Bityaeva <vbityaeva@gmail.com>
2025-04-07 16:40:38 +0300

    Убрала @Spy

--- src/test/java/FelineTest.java
+++ src/test/java/FelineTest.java
@@ -1,35 +1,34 @@
 import com.example.*;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.Spy;
+import org.mockito.Mockito;
 import org.mockito.junit.MockitoJUnitRunner;
 import org.junit.Assert;
-
 import java.util.Arrays;
-
-import static org.mockito.Mockito.when;
+import java.util.List;
+import static org.mockito.Mockito.doReturn;
 
 @RunWith(MockitoJUnitRunner.class)
 public class FelineTest {
 
-    @Spy
-    Feline feline;
-
     @Test
     public void testEatMeat() throws Exception {
-        Assert.assertEquals(Arrays.asList("Животные", "Птицы", "Рыба"), feline.eatMeat());
+        Feline spyFeline = Mockito.spy(new Feline());
+        List<String> expectedFood = Arrays.asList("Животные", "Птицы", "Рыба");
+        doReturn(expectedFood).when(spyFeline).getFood("Хищник");
+        Assert.assertEquals(expectedFood, spyFeline.eatMeat());
     }
 
     @Test
     public void testGetKittens() {
-        when(feline.getKittens(1)).thenReturn(3);  // Мокируем getKittens(int), чтобы вернуть 3
-        Assert.assertEquals(3, feline.getKittens());
+        Feline feline = new Feline();
+        Assert.assertEquals(1, feline.getKittens());
+        Assert.assertEquals(3, feline.getKittens(3));
     }
 
     @Test
     public void testGetFamily() {
+        Feline feline = new Feline();
         Assert.assertEquals("Кошачьи", feline.getFamily());
     }
 }
